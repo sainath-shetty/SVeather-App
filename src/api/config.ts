82b2e360952adc28f1b193e775ac0201c1441257
sainath-shetty <86493033+sainath-shetty@users.noreply.github.com>
2025-04-02 18:00:43 +0530

    modified config.ts

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,11 +1,11 @@
 export const API_CONFIG={
     BASE_URL:"https://api.openweathermap.org/data/2.5",
     GEO:"http://api.openweathermap.org/geo/1.0",
-    // API_KEY: import.meta.env.VITE_OPENWEATHER_API_KEY,
-    API_KEY: "12ecb087762ef00676b2d99b401e5737",
+    API_KEY: import.meta.env.VITE_OPENWEATHER_API_KEY,
+    
     DEFAULT_PARAMS:{
         units:"metric",
-        // appid:import.meta.env.VITE_OPENWEATHER_API_KEY,
-        appid:"12ecb087762ef00676b2d99b401e5737"
+        appid:import.meta.env.VITE_OPENWEATHER_API_KEY,
+        
     }
 };
